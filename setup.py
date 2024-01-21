46421d1f02c3dcb0c9761aa24ea0b831127e0d0a
keegang6705 <darunphobwi@gmail.com>
2024-01-21 11:32:11 +0700

    reupload all

--- setup.py
+++
@@ -1,8 +0,0 @@
-from cx_Freeze import setup, Executable
-
-setup(
-    name="BemisTyper By Keegang",
-    version="1.0",
-    description="Bemis auto-typer",
-    executables=[Executable("BemisTyper.py")],
-)
